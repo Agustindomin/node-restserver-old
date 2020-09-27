fded9ddf2b032af21c22d7b4e6df17daaaa28db8
Agustín <agustindomin@gmail.com>
2020-09-27 22:44:12 +0200

    Primer commit

---
+++ server/config/config.js
@@ -0,0 +1,5 @@
+// =============
+// Puerto
+// =============
+// Usamos el objeto global process
+process.env.PORT = process.env.PORT || 3000; // si no tiene un puerto, como en local, le ponemos 3000
---
+++ server/server.js
@@ -0,0 +1,58 @@
+// Cargamos el config
+require('./config/config');
+
+const express = require('express');
+const app = express();
+
+// Paquete body-parser para gestionar peticiones post (variables en el body de la petición)
+const bodyParser = require('body-parser')
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/usuario', function(req, res) {
+    // res.send('Hello World');
+    res.json('GET Usuario');
+});
+
+app.post('/usuario', function(req, res) {
+
+    let body = req.body;
+
+    if (body.nombre === undefined) {
+
+        res.status(400).json({
+            ok: false,
+            mensaje: 'El nombre es necesario'
+        }); // Si no viene el nombre devolvemos 400 - Bad Rsequest
+
+    } else {
+        res.json({
+            'persona': body
+        });
+    }
+
+});
+
+app.put('/usuario/:id', function(req, res) {
+
+    let id = req.params.id;
+
+    res.json({
+        id
+    });
+});
+
+app.delete('/usuario', function(req, res) {
+    res.json('POST Usuario');
+});
+
+// app.listen(3000, () => {
+//     console.log('Escuchando el puerto:', 3000);
+// });
+app.listen(process.env.PORT, () => {
+    console.log('Escuchando el puerto:', process.env.PORT);
+});
