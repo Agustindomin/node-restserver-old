cbd8819a21439fef4f004a607fb9be2f683b89c9
Agustín <agustindomin@gmail.com>
2020-10-09 20:28:08 +0200

    Variable de entorno MONGO_URI

--- server/config/config.js
+++ server/config/config.js
@@ -17,6 +17,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://Fagucito:StVcPRjKm6naQlHW@cluster0.c44pi.mongodb.net/cafe?retryWrites=true&w=majority';
+    urlDB = process.env.MONG0_URI;
 }
 process.env.URLDB = urlDB;
